491ce0ed45202b6478639d2ec04c3497d554de61
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-15 14:17:34 +0100

    add command CreateInventoryItem

---
+++ src/main/java/CreateInventoryItem.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class CreateInventoryItem {
+
+    public final UUID inventoryItemId;
+    public final String name;
+
+    public CreateInventoryItem(UUID inventoryItemId, String name) {
+        this.inventoryItemId = inventoryItemId;
+        this.name = name;
+    }
+}
