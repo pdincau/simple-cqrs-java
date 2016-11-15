e0c754ba641a010623034d34d629eba2968f2634
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-15 14:15:46 +0100

    add command DeactiveInventoryItem

---
+++ src/main/java/DeactivateInventoryItem.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class DeactivateInventoryItem {
+
+    public final UUID inventoryItemId;
+    public final int originalVersion;
+
+    public DeactivateInventoryItem(UUID inventoryItemId, int originalVersion) {
+        this.inventoryItemId = inventoryItemId;
+        this.originalVersion = originalVersion;
+    }
+}
