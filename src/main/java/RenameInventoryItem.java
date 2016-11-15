e6a986426e5ad054005d6a88df121f0ab8b348a3
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-15 14:20:48 +0100

    add command RenameInventoryItem

---
+++ src/main/java/RenameInventoryItem.java
@@ -0,0 +1,14 @@
+import java.util.UUID;
+
+public class RenameInventoryItem {
+
+    public final UUID inventoryItemId;
+    public final String newName;
+    public final int originalVersion;
+
+    public RenameInventoryItem(UUID inventoryItemId, String newName, int originalVersion) {
+        this.inventoryItemId = inventoryItemId;
+        this.newName = newName;
+        this.originalVersion = originalVersion;
+    }
+}
