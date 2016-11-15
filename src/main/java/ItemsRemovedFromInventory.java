b990325bea47a17caca88f39aafc59b23fa6c9f4
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-15 17:28:38 +0100

    add event ItemsRemovedFromInventory

---
+++ src/main/java/ItemsRemovedFromInventory.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class ItemsRemovedFromInventory {
+
+    public final UUID id;
+    public final int count;
+
+    public ItemsRemovedFromInventory(UUID id, int count) {
+        this.id = id;
+        this.count = count;
+    }
+}
