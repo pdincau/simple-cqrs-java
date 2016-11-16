0ef6804d616623f188453fec7ce7b11f364f7198
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-16 11:45:48 +0100

    add class EventStore

---
+++ src/main/java/EventStore.java
@@ -0,0 +1,11 @@
+import events.Event;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EventStore {
+
+    void saveEvents(UUID aggregateId, List<Event> events, int expectedVersion);
+
+    List<Event> getEventsForAggregate(UUID aggregateId);
+}
