600d15ab391e15ad653912199fed0b63f35d7a89
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-17 11:26:04 +0100

    change interface signature

--- src/main/java/AggregateRootRepository.java
+++ src/main/java/AggregateRootRepository.java
@@ -2,7 +2,7 @@ import java.util.UUID;
 
 public interface AggregateRootRepository<T extends AggregateRoot> {
 
-    void save(AggregateRoot aggregate, int expectedVersion);
+    void save(T aggregate, int expectedVersion);
 
     T getById(UUID id);
 }
