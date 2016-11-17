2b5df54966aa081b651686e89ff61ca559d58135
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-17 11:59:28 +0100

    add class InventoryCommandHandlers

--- src/main/java/InventoryCommandHandlers.java
+++ src/main/java/InventoryCommandHandlers.java
@@ -1,6 +1,39 @@
+import commands.*;
+
 public class InventoryCommandHandlers {
 
     private final AggregateRootRepository<InventoryItem> repository;
 
-    
+    public InventoryCommandHandlers(AggregateRootRepository<InventoryItem> repository) {
+        this.repository = repository;
+    }
+
+    public void handle(CreateInventoryItem command) {
+        InventoryItem item = new InventoryItem(command.inventoryItemId, command.name);
+        repository.save(item, -1);
+    }
+
+    public void handle(RenameInventoryItem command) {
+        InventoryItem item = repository.getById(command.inventoryItemId);
+        item.changeName(command.newName);
+        repository.save(item, command.originalVersion);
+    }
+
+    public void handle(RemoveItemsFromInventory command) {
+        InventoryItem item = repository.getById(command.inventoryItemId);
+        item.remove(command.count);
+        repository.save(item, command.originalVersion);
+    }
+
+    public void handle(CheckInItemsToInventory command) {
+        InventoryItem item = repository.getById(command.inventoryItemId);
+        item.checkIn(command.count);
+        repository.save(item, command.originalVersion);
+    }
+
+    public void handle(DeactivateInventoryItem command) {
+        InventoryItem item = repository.getById(command.inventoryItemId);
+        item.deactivate();
+        repository.save(item, command.originalVersion);
+    }
 }
