801bd33c97b062408c4b2fad88364121449faa85
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-20 12:54:35 +0100

    add class CommandBus

---
+++ src/main/java/domain/CommandBus.java
@@ -0,0 +1,12 @@
+package domain;
+
+import commands.Command;
+import commands.CommandSender;
+
+public class CommandBus implements CommandSender {
+    
+    @Override
+    public void send(Command command) {
+
+    }
+}
