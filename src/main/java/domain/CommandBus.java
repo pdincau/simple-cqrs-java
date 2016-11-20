4b75809fe8392c37a7943f709d915332e5653211
Paolo D'Incau <paolo.dincau@gmail.com>
2016-11-20 13:06:37 +0100

    pass handler to command bus so that it can be registered

--- src/main/java/domain/CommandBus.java
+++ src/main/java/domain/CommandBus.java
@@ -3,6 +3,7 @@ package domain;
 import com.google.common.eventbus.AsyncEventBus;
 import commands.Command;
 import commands.CommandSender;
+import commands.InventoryCommandHandlers;
 
 import java.util.concurrent.ExecutorService;
 
@@ -13,8 +14,9 @@ public class CommandBus implements CommandSender {
     private static final ExecutorService threadPool = newCachedThreadPool();
     private final AsyncEventBus bus;
 
-    public CommandBus() {
+    public CommandBus(InventoryCommandHandlers handler) {
         this.bus = new AsyncEventBus(threadPool);
+        this.bus.register(handler);
     }
 
     @Override
